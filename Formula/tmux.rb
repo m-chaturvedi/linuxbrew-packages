6a5af85d63a034e05fde11bdf09a4f0dd66384f1
Mmanu Chaturvedi <mmanu.chaturvedi@gmail.com>
2021-02-17 20:16:03 +0530

    Update

--- Formula/tmux.rb
+++ Formula/tmux.rb
@@ -3,18 +3,11 @@ class Tmux < Formula
   homepage ""
   url "https://github.com/tmux/tmux/releases/download/3.1c/tmux-3.1c.tar.gz"
   sha256 "918f7220447bef33a1902d4faff05317afd9db4ae1c9971bef5c787ac6c88386"
-  license ""
-
-  # depends_on "cmake" => :build
 
   def install
     # ENV.deparallelize
     system "./configure", "--prefix=#{prefix}"
-    system "make", "-j8"
+    system "make", "-j", Hardware::CPU.cores.to_s
     system "make", "install"
-  end
-
-  test do
-    system "false"
   end
 end
