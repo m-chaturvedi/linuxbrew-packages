d4e07f9ca0198a11a3ffc3dd78acfbaf9e41961f
Mmanu Chaturvedi <mmanu.chaturvedi@gmail.com>
2021-02-17 20:39:47 +0530

    Update

--- Formula/nvim.rb
+++ Formula/nvim.rb
@@ -1,7 +1,8 @@
 class Nvim < Formula
   desc "Installs Nvim"
   homepage ""
-  url "https://github.com/neovim/neovim/releases/download/$VERSION/nvim-linux64.tar.gz"
+  VERSION = "v0.4.4"
+  url "https://github.com/neovim/neovim/releases/download/#{VERSION}/nvim-linux64.tar.gz"
   sha256 "aeedee626c15c2e7460934f090fd4811cbb0fbffcd3e1e35fc6f7c06a9252a4a"
 
   def install
