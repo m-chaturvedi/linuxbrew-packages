3e54fc77ef1f7710e87e544dbe03c83a09ff84e2
Mmanu Chaturvedi <mmanu.chaturvedi@gmail.com>
2021-02-17 20:57:48 +0530

    Update

--- Formula/nvim.rb
+++ Formula/nvim.rb
@@ -6,7 +6,8 @@ class Nvim < Formula
   sha256 "aeedee626c15c2e7460934f090fd4811cbb0fbffcd3e1e35fc6f7c06a9252a4a"
 
   def install
-    mv "bin", "share", "#{prefix}"
+    mv "bin", "#{prefix}"
+    mv "share", "#{prefix}"
   end
 end
 
