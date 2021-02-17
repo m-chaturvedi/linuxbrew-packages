928496cd990dca72e71fbf5e4b28608818ea6da7
Mmanu Chaturvedi <mmanu.chaturvedi@gmail.com>
2021-02-17 20:36:18 +0530

    Update

---
+++ nvim.rb
@@ -0,0 +1,10 @@
+class NVIM < Formula
+  desc "Installs Nvim"
+  homepage ""
+  url "https://github.com/neovim/neovim/releases/download/$VERSION/nvim-linux64.tar.gz"
+  sha256 "aeedee626c15c2e7460934f090fd4811cbb0fbffcd3e1e35fc6f7c06a9252a4a"
+
+  def install
+  end
+end
+
