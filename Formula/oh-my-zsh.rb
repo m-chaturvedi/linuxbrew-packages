6cd2897c8a3ab234190edb15f89b505f65371ee7
Mmanu Chaturvedi <mmanu.chaturvedi@gmail.com>
2021-02-25 19:46:39 +0530

    Update oh my zsh

--- Formula/oh-my-zsh.rb
+++ Formula/oh-my-zsh.rb
@@ -1,6 +1,6 @@
 class OhMyZsh < Formula
   desc "Installs Oh My ZSH"
-  url "git@github.com:ohmyzsh/ohmyzsh.git", :using => :git, :tag => "9a9f383"
+  url "git@github.com:ohmyzsh/ohmyzsh.git", :using => :git, :tag => "9a9f38319"
 
   def install
     system "/bin/bash", "tools/install.sh"
